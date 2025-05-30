854380a91c1cad7d3857179f41f9d059fa56a397
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-05-30 12:57:47 +0400

    font family added

--- src/Components/NavSlider/NavSlider.jsx
+++ src/Components/NavSlider/NavSlider.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-fade';
-import sliderImg1 from './assets/slider-6.jpg';
-import sliderImg2 from './assets/slider-5.jpg'
-import sliderImg3 from './assets/slider-4.jpg'
-import slide from './NavSlider.module.css';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import sliderImg1 from "./assets/slider-6.jpg";
+import sliderImg2 from "./assets/slider-5.jpg";
+import sliderImg3 from "./assets/slider-4.jpg";
+import slide from "./NavSlider.module.css";
+import imgRotate from "./assets/text-rotate-3.png"
 
 function NavSlider() {
   return (
@@ -28,15 +29,18 @@ function NavSlider() {
         <SwiperSlide>
           <div
             className={slide.slide}
-            
             style={{ backgroundImage: `url(${sliderImg1})` }}
           >
             <div className={slide.content}>
-            <p>Hot Sale 50% Discount</p>
-              <h2>Bay a planter,get a grow light free
-              </h2>
+              <h2>Bay a planter,get a grow light free</h2>
+              <p>
+                From Modern Sprout’s clever grow kits to Laila Solomon’s
+                handmade ceramic planters, discover the products created by
+                talented artisans and curated by us in our Fall Collection
+              </p>
               <div className={slide.sliButton}>Buy Now</div>
             </div>
+            <img src={imgRotate} className={slide.imgRot}/>
           </div>
         </SwiperSlide>
 
@@ -46,10 +50,15 @@ function NavSlider() {
             style={{ backgroundImage: `url(${sliderImg2})` }}
           >
             <div className={slide.content}>
-            <p>Make Your Dream House</p>
               <h2>Bring The Green World to Your Home</h2>
+              <p>
+                From Modern Sprout’s clever grow kits to Laila Solomon’s
+                handmade ceramic planters, discover the products created by
+                talented artisans and curated by us in our Fall Collection
+              </p>
               <div className={slide.sliButton}>Buy Now</div>
             </div>
+              <img src={imgRotate} className={slide.imgRot}/>
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -58,10 +67,15 @@ function NavSlider() {
             style={{ backgroundImage: `url(${sliderImg3})` }}
           >
             <div className={slide.content}>
-            <p>Make Your Dream House</p>
               <h2>Bring The Green World to Your Home</h2>
+              <p>
+                From Modern Sprout’s clever grow kits to Laila Solomon’s
+                handmade ceramic planters, discover the products created by
+                talented artisans and curated by us in our Fall Collection
+              </p>
               <div className={slide.sliButton}>Buy Now</div>
             </div>
+              <img src={imgRotate} className={slide.imgRot}/>
           </div>
         </SwiperSlide>
       </Swiper>
@@ -70,4 +84,3 @@ function NavSlider() {
 }
 
 export default NavSlider;
-
